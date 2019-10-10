5763b98a292a0af877653f6a92461abf50a7cd4b
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-10 15:32:04 +0200

    added Contact list

---
+++ src/components/ContactList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const userTab = [
+    {
+        name : 'Rosemary Welch',
+        avatar : 'https://randomuser.me/api/portraits/women/90.jpg',
+        online : true
+        
+        
+    },
+    {
+        name : 'Ava Bell',
+        avatar : 'https://randomuser.me/api/portraits/women/28.jpg',
+        online : true
+        
+    },
+    {
+        name :'Teresa Ferguson',
+        avatar : 'https://randomuser.me/api/portraits/women/40.jpg',
+        online : false
+        
+    },
+    {
+        name :'Aiden Jimenez',
+        avatar : 'https://randomuser.me/api/portraits/men/64.jpg',
+        online : true
+        
+    },
+    {
+        name : 'Monica Willis',
+        avatar : 'https://randomuser.me/api/portraits/women/15.jpg',
+        online : false
+        
+    }
+];
+
+
+
+
+export default ContactList;
